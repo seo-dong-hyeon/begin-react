f372b44382bbafcfdf92b1ff5fca490e25796659
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2020-08-30 23:36:40 +0900

    useRef -> 값 변화 관리

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React , { useRef } from 'react';
 import UserList from './UserList';
 
 function App() {
+  const users = [
+    {
+        id: 1,
+        username: 'test1',
+        email: 'test1@gmail.com'
+    },
+    {
+        id: 2,
+        username: 'test2',
+        email: 'test2@gmail.com'
+    },
+    {
+        id: 3,
+        username: 'test3',
+        email: 'test3@gmail.com'
+    },
+  ];
+
+  const nextId = useRef(4); // id가 바뀌어도 컴포넌트가 리렌더링 될 필요는 없음 -> useState x, useRef
+  const onCreate = () => {
+    nextId.current += 1; // 컴포넌트가 리렌더링 되더라도 바뀐 값이 유지
+  }
+
   return (
-    <UserList />
+    <UserList users={users} />
   );
 }
 
--- src/UserList.js
+++ src/UserList.js
@@ -8,30 +8,12 @@ function User({user}){
     );
 }
 
-function UserList(){
-    const users = [
-        {
-            id: 1,
-            username: 'test1',
-            email: 'test1@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'test2',
-            email: 'test2@gmail.com'
-        },
-        {
-            id: 3,
-            username: 'test3',
-            email: 'test3@gmail.com'
-        },
-    ];
-
+function UserList({users}){
     return (
         <div>
             {
                 users.map(
-                    user => (<User user={user} key={user.id} />)
+                    user => (<User user={user} key={user.id} />) // index값 말고 별도의 key값으로
                 )
             }
         </div>
