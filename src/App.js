18cd51e9b54303ff34a36adf115800c231d3fb7e
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2020-08-30 23:54:04 +0900

    배열에 새로운 요소 추가하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React , { useRef } from 'react';
+import React , { useRef, useState } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
         id: 1,
         username: 'test1',
@@ -18,15 +32,33 @@ function App() {
         username: 'test3',
         email: 'test3@gmail.com'
     },
-  ];
+  ]);
 
   const nextId = useRef(4); // id가 바뀌어도 컴포넌트가 리렌더링 될 필요는 없음 -> useState x, useRef
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]); // 기존 배열 복사 + 새로운 요소 추가 == user.concat(user)
+    setInputs({
+      username: '',
+      email: ''
+    })
     nextId.current += 1; // 컴포넌트가 리렌더링 되더라도 바뀐 값이 유지
   }
 
   return (
-    <UserList users={users} />
+    <>
+      <CreateUser 
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}  
+      />
+      <UserList users={users} />
+    </> 
   );
 }
 
